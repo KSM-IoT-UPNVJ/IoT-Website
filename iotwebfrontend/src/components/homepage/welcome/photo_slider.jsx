d688a60e10187a1cf1af44253e3db5455ab7ceb7
zafar2154 <zafar2154.ez@gmail.com>
2025-09-19 16:02:35 +0700

    slider selesai dan update foto di slider pada welcome

--- iotwebfrontend/src/components/homepage/welcome/photo_slider.jsx
+++ iotwebfrontend/src/components/homepage/welcome/photo_slider.jsx
@@ -1,35 +1,20 @@
-
-export default function PhotoSlider({
-  duration = 10,
-  images
-}) {
+export default function PhotoSlider({ images, direction }) {
   const all = [...images, ...images]; // duplicate for seamless loop
 
   return (
-    <div className="w-full overflow-hidden">
-      <div
-        className="flex w-max"
-        style={{
-          animation: `scrollRight ${duration}s linear infinite`,
-        }}
-      >
-        {all.map((src, i) => (
-          <div key={i} className="flex-shrink-0 p-2">
+    <div className="w-full overflow-hidden -z-10">
+      <div className={`flex w-max ${direction}`}>
+        {all.map((photo, index) => (
+          <div key={index} className="p-1.5">
             <img
-              src={src}
-              alt={`photo-${i}`}
-              className="w-56 h-40 object-cover rounded-2xl shadow-md"
+              src={photo}
+              alt={`photo-${index}`}
+              className="w-70 h-36 object-cover rounded-2xl"
+              loading="lazy"
             />
           </div>
         ))}
       </div>
-
-      <style>{`
-@keyframes scrollRight {
-0% { transform: translateX(-50%); }
-100% { transform: translateX(0%); }
-}
-`}</style>
     </div>
   );
 }
--- iotwebfrontend/src/components/homepage/welcome/welcome.jsx
+++ iotwebfrontend/src/components/homepage/welcome/welcome.jsx
@@ -1,21 +1,44 @@
+import { animate } from 'framer-motion';
 import FadeIn from '../../../utils/fadeIn';
 import PhotoSlider from './photo_slider';
 import 'swiper/css';
 
 const Home = () => {
-  const images = [
-    'aboutUs/azam.webp',
-    'aboutUs/bangDhoni.webp',
-    'aboutUs/bangAzriel.webp',
-    'aboutUs/bangMusfa.webp',
-    'aboutUs/bangFaiq.webp',
-    'aboutUs/bangRifqi.webp',
-    'aboutUs/bangZahid.webp',
-    
+  const imagesRow1 = [
+    'homepage/foto1.webp',
+    'homepage/foto2.webp',
+    'homepage/foto3.webp',
+    'homepage/foto4.webp',
+    'homepage/foto5.webp',
   ];
+  const imagesRow2 = [
+    'homepage/foto8.webp',
+    'homepage/foto9.webp',
+    'homepage/foto10.webp',
+    'homepage/foto11.webp',
+    'homepage/foto12.webp',
+  ];
+  const imagesRow3 = [
+    'homepage/foto15.webp',
+    'homepage/foto16.webp',
+    'homepage/foto17.webp',
+    'homepage/foto18.webp',
+    'homepage/foto19.webp',
+  ];
+  const imagesRow4 = [
+    'homepage/foto22.webp',
+    'homepage/foto23.webp',
+    'homepage/foto21.webp',
+    'homepage/foto14.webp',
+    'homepage/foto20.webp',
+  ];
+
   return (
-    <div>
-      <PhotoSlider images={images} />
+    <div className="flex flex-col w-full">
+      <PhotoSlider images={imagesRow1} direction="animate-scroll-right" />
+      <PhotoSlider images={imagesRow2} direction="animate-scroll-left" />
+      <PhotoSlider images={imagesRow3} direction="animate-scroll-right" />
+      <PhotoSlider images={imagesRow4} direction="animate-scroll-left" />
     </div>
   );
 };
