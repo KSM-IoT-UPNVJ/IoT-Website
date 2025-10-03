d821dbecb6bad345732773c6fd532060b2848b3e
zafar2154 <zafar2154.ez@gmail.com>
2025-10-03 21:19:43 +0700

    fix:fix nav navigate

--- iotwebfrontend/src/components/nav.jsx
+++ iotwebfrontend/src/components/nav.jsx
@@ -1,8 +1,8 @@
-import { useState, useEffect, useRef } from "react";
-import { ChevronDown, Menu, X } from "lucide-react";
-import FadeIn from "../utils/fadeIn";
-import DropdownAnimate from "../utils/dropdownAnimate";
-import { useNavigate } from "react-router-dom";
+import { useState, useEffect, useRef } from 'react';
+import { ChevronDown, Menu, X } from 'lucide-react';
+import FadeIn from '../utils/fadeIn';
+import DropdownAnimate from '../utils/dropdownAnimate';
+import { useNavigate } from 'react-router-dom';
 
 function Nav() {
   const navigate = useNavigate();
@@ -20,30 +20,30 @@ function Nav() {
       }
     };
 
-    document.addEventListener("mousedown", handleClickOutside);
+    document.addEventListener('mousedown', handleClickOutside);
 
     return () => {
-      document.removeEventListener("mousedown", handleClickOutside);
+      document.removeEventListener('mousedown', handleClickOutside);
     };
   }, []);
 
   const linkClass =
-    "flex text-md cursor-pointer justify-center font-medium items-center px-4 hover:-translate-y-0.5 transition transform duration-100 hover:text-biru-sedang";
+    'flex text-md cursor-pointer justify-center font-medium items-center px-4 hover:-translate-y-0.5 transition transform duration-100 hover:text-biru-sedang';
 
   const mobileLinkClass =
-    "block text-md px-4 py-2 hover:bg-abu-sedang rounded-xl transition";
+    'block text-md px-4 py-2 hover:bg-abu-sedang rounded-xl transition';
   return (
     <div className="sticky bg-gradient-to-r from-biru-muda to-white shadow-md py-6.5 px-7.5 w-full h-auto">
       <div className="flex justify-between items-center">
         {/* logo */}
         <div className="flex items-center space-x-2">
           <div>
-            <FadeIn delay={1.2} direction={"left"}>
+            <FadeIn delay={1.2} direction={'left'}>
               <img src="Logo_IoT.png" alt="logo" className="w-auto h-10" />
             </FadeIn>
           </div>
           <div>
-            <FadeIn delay={1.2} direction={"left"}>
+            <FadeIn delay={1.2} direction={'left'}>
               <p className="font-titillium font-semibold text-base ">
                 KSM Internet Of Things
               </p>
@@ -56,17 +56,22 @@ function Nav() {
 
         {/* Navigation list */}
         <div className="hidden md:flex justify-center items-center space-x-2">
-          <a href="home" className={linkClass}>
-            <FadeIn delay={0.3} direction={"down"}>
+          <button
+            onClick={() => {
+              navigate(`/home`);
+            }}
+            className={linkClass}
+          >
+            <FadeIn delay={0.3} direction={'down'}>
               Home
             </FadeIn>
-          </a>
+          </button>
           <div ref={dropdownRef}>
             <button onClick={handleToggle} className={linkClass}>
-              <FadeIn delay={0.4} direction={"down"}>
+              <FadeIn delay={0.4} direction={'down'}>
                 About
               </FadeIn>
-              <FadeIn delay={0.4} direction={"down"}>
+              <FadeIn delay={0.4} direction={'down'}>
                 <ChevronDown size={16} />
               </FadeIn>
             </button>
@@ -79,26 +84,28 @@ function Nav() {
                   >
                     About Us
                   </button>
-                  <a
-                    href="kepengurusan"
+                  <button
+                    onClick={() => {
+                      navigate(`/kepengurusan`);
+                    }}
                     className="block text-md text-center rounded-xl px-2 py-2 hover:-translate-y-0.5 transition transform duration-100 hover:bg-abu-sedang"
                   >
                     Kepengurusan
-                  </a>
+                  </button>
                 </DropdownAnimate>
               </div>
             )}
           </div>
-          <a href="project" className={linkClass}>
-            <FadeIn delay={0.8} direction={"down"}>
+          <button onClick={() => navigate(`/project`)} className={linkClass}>
+            <FadeIn delay={0.8} direction={'down'}>
               Projects
             </FadeIn>
-          </a>
-          <a href="contact" className={linkClass}>
-            <FadeIn delay={1.0} direction={"down"}>
+          </button>
+          <button onClick={() => navigate(`/contact`)} className={linkClass}>
+            <FadeIn delay={1.0} direction={'down'}>
               Contact
             </FadeIn>
-          </a>
+          </button>
         </div>
 
         <div className="md:hidden">
@@ -108,20 +115,20 @@ function Nav() {
         </div>
 
         <div className="hidden md:block">
-          <FadeIn delay={1.2} direction={"down"}>
+          <FadeIn delay={1.2} direction={'down'}>
             <img src="slogan.png" alt="slogan" className="w-auto h-10" />
           </FadeIn>
         </div>
       </div>
       {isMobileMenuOpen && (
         <div className="mt-4 md:hidden bg-white p-4 rounded-xl shadow border border-biru-muda space-y-2 animate-fade-in">
-          <a
+          <button
+            onClick={() => navigate(`/home`)}
             href="home"
             className={mobileLinkClass}
-            onClick={handleToggleMobileMenu}
           >
             Home
-          </a>
+          </button>
           <div ref={dropdownRef}>
             <button
               onClick={handleToggle}
@@ -132,38 +139,45 @@ function Nav() {
             {open && (
               <DropdownAnimate show={open} className="mt-2 space-y-1">
                 <div className="px-5 border-1 border-biru-muda bg-white rounded-xl">
-                  <a
-                    href="aboutus"
+                  <button
+                    onClick={() => {
+                      navigate(`/aboutus`);
+                    }}
                     className={mobileLinkClass}
-                    onClick={handleToggleMobileMenu}
                   >
                     About Us
-                  </a>
-                  <a
+                  </button>
+                  <button
+                    onClick={() => {
+                      navigate(`/kepengurusan`);
+                    }}
                     href="kepengurusan"
                     className={mobileLinkClass}
-                    onClick={handleToggleMobileMenu}
                   >
                     Kepengurusan
-                  </a>
+                  </button>
                 </div>
               </DropdownAnimate>
             )}
           </div>
-          <a
-            href="project"
+          <button
+            onClick={() => {
+              navigate(`/project`);
+              handleToggleMobileMenu();
+            }}
             className={mobileLinkClass}
-            onClick={handleToggleMobileMenu}
           >
             Projects
-          </a>
-          <a
-            href="contact"
+          </button>
+          <button
+            onClick={() => {
+              navigate(`/contact`);
+              handleToggleMobileMenu();
+            }}
             className={mobileLinkClass}
-            onClick={handleToggleMobileMenu}
           >
             Contact
-          </a>
+          </button>
         </div>
       )}
     </div>
