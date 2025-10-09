4ea6db982b62388add586dd863595503a970d97a
PoH-Hole <2410314079@mahasiswa.upnvj.ac.id>
2025-10-09 21:40:35 +0700

    benerin ukuran

--- iotwebfrontend/src/components/AboutUs/WhatIs.jsx
+++ iotwebfrontend/src/components/AboutUs/WhatIs.jsx
@@ -19,10 +19,10 @@ const WhatIs = () => {
       </FadeIn>
 
       <FadeIn direction={'right'} delay={0.8}>
-        <div className="flex flex-col xl:flex-row items-start justify-center gap-10 md:gap-20 px-4 md:px-10">
+        <div className="flex flex-col xl:flex-row items-start justify-center gap-10 px-4 md:px-10">
           {/* Embed YouTube Video */}
-          <div className="w-full xl:w-1/3 mt-10 flex justify-center select-none">
-            <div className="w-full max-w-[600px] aspect-video rounded-2xl overflow-hidden shadow-lg">
+          <div className="w-full xl:w-3/5 mt-10 md:mt-23 flex justify-center select-none">
+            <div className="w-full max-w-[750px] aspect-video rounded-2xl overflow-hidden shadow-lg">
               <iframe
                 className="w-full h-full"
                 src="https://www.youtube.com/embed/mGKpeLC5oSk?si=6X69cpgPUfhUXgE4"
